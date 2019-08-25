5841b9591f6c6fb0e2b55c16837cfaa112800579
somya guglani <somyaguglani0897@gmail.com>
2019-08-25 19:26:35 +0530

    My first Commit

---
+++ app/src/main/java/com/example/mypracticerightnowapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.mypracticerightnowapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.action_about:
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+                break;
+                case R.id.action_more:
+                Toast.makeText(this, "More", Toast.LENGTH_SHORT).show();
+                break;
+                case R.id.action_refresh:
+                Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+                break;
+                case R.id.action_search:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return false;
+    }
+}
